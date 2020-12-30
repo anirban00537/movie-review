a42e5985a54b0ab990943eb1f27fe94f29cc1e3e
anirban00537 <34684592+anirban00537@users.noreply.github.com>
2020-12-30 16:31:22 +0600

    design commit

--- client/src/components/home/Right_sidebar.js
+++ client/src/components/home/Right_sidebar.js
@@ -3,33 +3,21 @@ import "./right_sidebar.css";
 const Right_sidebar = () => {
   return (
     <div className="sidebar">
-      <h3 className="headline">Top rated movies</h3>
+      <h3 className="headline">Recent Released</h3>
       <div className="size">
-        <img
-          src="https://cdn.pixabay.com/photo/2020/03/01/12/36/fire-4892711_960_720.jpg"
-          className="top_image"
-        />
-        <h4>Solay</h4>
-        <p className="paragraph">
-          ₹350 million (India) 60 million tickets (USSR) Sholay (Hindustani:
-          [ˈʃoːleː] ( listen), transl. Embers) is a 1975 Indian action-adventure
-          film written by Salim–Javed, directed by Ramesh Sippy, and produced by
-          his father G. P.
-        </p>
-      </div>
+        <div className="image_Section">
+          <img
+            src="https://cdn.pixabay.com/photo/2020/03/01/12/36/fire-4892711_960_720.jpg"
+            className="top_image"
+          />
+        </div>
 
-      <div className="size">
-        <img
-          src="https://cdn.pixabay.com/photo/2020/03/01/12/36/fire-4892711_960_720.jpg"
-          className="top_image"
-        />
-        <h4>Solay</h4>
-        <p className="paragraph">
-          ₹350 million (India) 60 million tickets (USSR) Sholay (Hindustani:
-          [ˈʃoːleː] ( listen), transl. Embers) is a 1975 Indian action-adventure
-          film written by Salim–Javed, directed by Ramesh Sippy, and produced by
-          his father G. P.
-        </p>
+        <div className="descriptions_section">
+          <h4>Solay</h4>
+          <p className="paragraph">
+            ₹350 million (India) 60 million tickets (USSR) Sholay (Hindustani:
+          </p>
+        </div>
       </div>
     </div>
   );
